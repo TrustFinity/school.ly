7a7110ccbc3b08ce23d02628627cffcccde32af4
mwakaambrose <ambrosemwaka@gmail.com>
2018-01-08 21:07:17 +0300

    edit the index listings of levels

--- resources/views/levels/index.blade.php
+++ resources/views/levels/index.blade.php
@@ -1,39 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-
-        <a href="/levels/create"><button type="button" class="btn btn-lg btn-info">Add a new level</button></a>
-        <hr><br>
-            <table class="table table-bordered table-hover">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Has the follwing subjects</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach ($levels as $level)
-                    <tr>
-                        <td>{{ $level->id }}</td>
-                        <td>{{ $level->name }}</td>
-                        <td>
-                        @foreach ($level->subjects as $subject) 
-                        <li>{{ $subject->name }}</li>
-                        @endforeach
-                        </td>
-                        <td><a href="/levels/edit/{{ $level->id }}"><button type="button" class="btn btn-priamry">Edit</button></a></td>
-                        <td><a href="/levels/delete/{{ $level->id }}"><button type="button" class="btn btn-danger">Delete</button></a></td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
+        <div class="col-sm-6">
+            <h3 class="text-info">Levels</h3>
+            <p>A listing of all levels in your {{ getPreference()->institution_type }}. this can be applied to secondary schools and it is optional.</p>
+        </div>
+        <div class="col-sm-6">
+            <a href="/levels/create" class="btn btn-success pull-right">Add new Level</a>
         </div>
     </div>
-</div>
+    <hr>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="center-block">
+                <div class="row">
+                    <div class="col-xs-2">
+                        <h4>Level Name</h4>
+                    </div>
+                    <div class="col-xs-5">
+                        <h4>Has dive following subjects</h4>
+                    </div>
+                    <div class="col-xs-2 pull-right">
+                        <h4>Actions</h4>
+                    </div>
+                </div>
+                @foreach ($levels as $level)
+                    <div class="row">
+                        <div class="col-xs-2">
+                            {{ $level->name }}
+                        </div>
+                        <div class="col-xs-5">
+                            @foreach ($level->subjects as $subject)
+                                <p class="badge">{{ $subject->name }}</p>
+                            @endforeach
+                                <hr>
+                        </div>
+                        <div class="col-xs-2 pull-right">
+                            <div class="btn-group">
+                                <a href="/levels/edit/{{ $level->id }}" class="btn btn-default">Edit</a>
+                                <a href="/levels/delete/{{ $level->id }}" class="btn btn-default">Delete</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 @endsection
