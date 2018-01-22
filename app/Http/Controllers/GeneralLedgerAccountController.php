011b4898b4186e8d84eb51293e84f4dbcd5da0bc
mwakaambrose <ambrosemwaka@gmail.com>
2018-01-22 17:47:26 +0300

    stubs gla data

---
+++ app/Http/Controllers/GeneralLedgerAccountController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\GeneralLedgerAccount;
+use Illuminate\Http\Request;
+
+class GeneralLedgerAccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\GeneralLedgerAccount  $generalLedgerAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GeneralLedgerAccount $generalLedgerAccount)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\GeneralLedgerAccount  $generalLedgerAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(GeneralLedgerAccount $generalLedgerAccount)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\GeneralLedgerAccount  $generalLedgerAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GeneralLedgerAccount $generalLedgerAccount)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\GeneralLedgerAccount  $generalLedgerAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GeneralLedgerAccount $generalLedgerAccount)
+    {
+        //
+    }
+}
