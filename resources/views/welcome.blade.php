8cc4c788f4a75a6a3b544e10007b482e26121a97
mwakaambrose <ambrosemwaka@gmail.com>
2018-01-26 16:32:08 +0300

    finishes first index design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,138 +1,137 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="img/favicon.png">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="img/favicon.png">
+        <title>{{ config('app.name') }} | Mordern tools in your school</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/welcome.css" rel="stylesheet">
+    </head>
 
-    <title>{{ config('app.name') }} | Mordern tools in your school</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/welcome.css" rel="stylesheet">
-  </head>
-
-  <body>
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-        {{-- <div class="container"> --}}
+    <body>
+        <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
             <h5 class="my-0 mr-md-auto font-weight-bold">{{ config('app.name') }}</h5>
             <nav class="my-2 my-md-0 mr-md-3">
-                <a class="p-2 text-dark" href="#">Features</a>
-                <a class="p-2 text-dark" href="#">Support</a>
-                <a class="p-2 text-dark" href="#">Pricing</a>
+                <a class="p-2 text-dark" href="#footer">Features</a>
+                <a class="p-2 text-dark" href="#pricing">Pricing</a>
             </nav>
-            <a class="btn btn-outline-primary" href="/login">Sign in</a>
-        {{-- </div> --}}
-    </div>
-
-    <div class="position-relative overflow-hidden m-md-5 col-xs-6 col-xs-offset-3">
-        {{-- <div class="col-md-5 p-lg-5 mx-auto my-5"> --}}
-            <p class="lead font-weight-normal text-center">
-                For schools that aim to simplify creating, distributing and grading assignments in a paperless way.
-            </p>
-        {{-- </div> --}}
-        <div class="product-device product-device-2 box-shadow d-none d-md-block align-items-center">
-            <img src="/img/splash.png" alt="splash" class="card-img img-responsive splash-img">
+            <a class="btn btn-outline-primary" href="/login">SignIn to {{ config('app.name') }}</a>
         </div>
-    </div>
 
-    <hr class="row">
-        {{-- <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-faded">
-            mwaka
-        </div> --}}
-    {{-- <hr class="row"> --}}
-
-    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-      <h1 class="display-4">Pricing</h1>
-      <p class="lead">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.</p>
-    </div>
-
-    <div class="container">
-      <div class="card-deck mb-3 text-center">
-        <div class="card mb-4 box-shadow">
-          <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Free</h4>
-          </div>
-          <div class="card-body">
-            <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
-            <ul class="list-unstyled mt-3 mb-4">
-              <li>10 users included</li>
-              <li>2 GB of storage</li>
-              <li>Email support</li>
-              <li>Help center access</li>
-            </ul>
-            <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
-          </div>
-        </div>
-        <div class="card mb-4 box-shadow">
-          <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Pro</h4>
-          </div>
-          <div class="card-body">
-            <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
-            <ul class="list-unstyled mt-3 mb-4">
-              <li>20 users included</li>
-              <li>10 GB of storage</li>
-              <li>Priority email support</li>
-              <li>Help center access</li>
-            </ul>
-            <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
-          </div>
-        </div>
-        <div class="card mb-4 box-shadow">
-          <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Enterprise</h4>
-          </div>
-          <div class="card-body">
-            <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
-            <ul class="list-unstyled mt-3 mb-4">
-              <li>30 users included</li>
-              <li>15 GB of storage</li>
-              <li>Phone and email support</li>
-              <li>Help center access</li>
-            </ul>
-            <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
-          </div>
-        </div>
-      </div>
-
-      <footer class="pt-4 my-md-5 pt-md-5 border-top">
         <div class="row">
-          <div class="col-12 col-md">
-            <img class="mb-2" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
-            <small class="d-block mb-3 text-muted">&copy; 2017-2018</small>
-          </div>
-          <div class="col-6 col-md">
-            <h5>Features</h5>
-            <ul class="list-unstyled text-small">
-              <li><a class="text-muted" href="#">Cool stuff</a></li>
-              <li><a class="text-muted" href="#">Random feature</a></li>
-              <li><a class="text-muted" href="#">Team feature</a></li>
-              <li><a class="text-muted" href="#">Stuff for developers</a></li>
-              <li><a class="text-muted" href="#">Another one</a></li>
-              <li><a class="text-muted" href="#">Last time</a></li>
-            </ul>
-          </div>
-          <div class="col-6 col-md">
-            <h5>Resources</h5>
-            <ul class="list-unstyled text-small">
-              <li><a class="text-muted" href="#">Resource</a></li>
-              <li><a class="text-muted" href="#">Resource name</a></li>
-              <li><a class="text-muted" href="#">Another resource</a></li>
-              <li><a class="text-muted" href="#">Final resource</a></li>
-            </ul>
-          </div>
-          <div class="col-6 col-md">
-            <h5>About</h5>
-            <ul class="list-unstyled text-small">
-              <li><a class="text-muted" href="#">Team</a></li>
-              <li><a class="text-muted" href="#">Locations</a></li>
-              <li><a class="text-muted" href="#">Privacy</a></li>
-              <li><a class="text-muted" href="#">Terms</a></li>
-            </ul>
-          </div>
+            <div class="col"></div>
+            <div class="col">
+                <br><br>
+                <h1 class="text-center">Productivity at Scale</h1>
+                <br>
+                <h3 class="text-center lead">
+                    A management platform for schools that aim to simplify creating, distributing and grading assignments in a paperless way
+                </h3>
+                <br>
+                <img src="/img/splash.png" alt="" class="img-responsive">
+            </div>
+            <div class="col"></div>
         </div>
-      </footer>
-    </div>
-  </body>
+
+        <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+            <h1 class="display-4">Pricing</h1>
+            <p class="lead">
+                Always know what you'll pay with our yearly and flat pricing models for our base offers. Get {{ config('app.name') }} set up for your school
+            </p>
+        </div>
+
+        <div class="container">
+            <div class="card-deck mb-3 text-center">
+                
+                <div class="card mb-4 box-shadow">
+                    <div class="card-header">
+                        <h4 class="my-0 font-weight-normal">Base</h4>
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title pricing-card-title">$4<sup class="text-muted">99</sup><small class="text-muted">/yr</small></h1>
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li>less than 1000 users</li>
+                            <li>2 GB of storage</li>
+                            <li>Email support</li>
+                            <li>Help center access</li>
+                        </ul>
+                       <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
+                    </div>
+                </div>
+                <div class="card mb-4 box-shadow">
+                    <div class="card-header">
+                        <h4 class="my-0 font-weight-normal">Pro</h4>
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title pricing-card-title">$8<small class="text-muted">/yr</small></h1>
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li>1000 users plus</li>
+                            <li>10 GB of storage</li>
+                            <li>Priority email support</li>
+                            <li>Help center access</li>
+                        </ul>
+                        <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
+                    </div>
+                </div>
+                <div class="card mb-4 box-shadow">
+                  <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Enterprise</h4>
+                  </div>
+                    <div class="card-body">
+                        <p class="text-primary">Need something thats tuned to your needs? we can build that for you.</p>
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li>Custom built features</li>
+                            <li>Custom storage</li>
+                            <li>Phone and email support</li>
+                        </ul>
+                        <button type="button" class="btn btn-lg btn-block btn-outline-primary">Contact us</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <p class="text-center">We reserve the right to pricing changes without notice or consent</p>
+        <h3 class="text-center text-muted">+256 779 512013</h3>
+
+        <div class="container">
+            <footer class="pt-4 my-md-5 pt-md-5 border-top">
+                <div class="row">
+                    <div class="col-12 col-md">
+                        <img src="img/trustfinity-logo.png" height="50">
+                        <small class="d-block mb-3 text-muted">&copy; 2017-2018</small>
+                    </div>
+                    <div class="col-6 col-md">
+                        <h5>Features</h5>
+                        <ul class="list-unstyled text-small">
+                          <li><a class="text-muted">Reports</a></li>
+                          <li><a class="text-muted">Accounting</a></li>
+                          <li><a class="text-muted">Attendances</a></li>
+                          <li><a class="text-muted">Examinations</a></li>
+                          <li><a class="text-muted">Chart Of Accounts</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6 col-md">
+                        <h5>Resources</h5>
+                        <ul class="list-unstyled text-small">
+                          <li><a class="text-muted">Teachers</a></li>
+                          <li><a class="text-muted">Students &amp Pupils</a></li>
+                          <li><a class="text-muted">Support Staff</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6 col-md">
+                    <h5>About</h5>
+                        <ul class="list-unstyled text-small">
+                          <li><a class="text-muted">Team</a></li>
+                          <li><a class="text-muted">Terms</a></li>
+                          <li><a class="text-muted">Privacy</a></li>
+                          <li><a class="text-muted">Locations</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </body>
 </html>
