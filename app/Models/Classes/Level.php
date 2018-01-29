bb6afda20ba1ea34eb3ed3ec5cd862ce191126ab
mwakaambrose <ambrosemwaka@gmail.com>
2018-01-29 21:59:23 +0300

    fixes class groups

--- app/Models/Classes/Level.php
+++ app/Models/Classes/Level.php
@@ -3,11 +3,18 @@
 namespace App\Models\Classes;
 
 use App\Models\Student;
+use App\Scopes\SchoolScope;
 use Illuminate\Database\Eloquent\Model;
 
 class Level extends Model
 {
     protected $guarded = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new SchoolScope());
+    }
 
     public function classGroups()
     {
--- resources/views/class-groups/index.blade.php
+++ resources/views/class-groups/index.blade.php
@@ -2,45 +2,46 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-12">
-            <div class="col-sm-6">
-                <h4><b>Class Groups</b></h4>
-            </div>
-            <div class="col-sm-6">
-                <a href="/class-groups/create" class="btn btn-md btn-success pull-right">+ Add New Class Group</a>
-            </div>
-            <hr>
+        <div class="col-sm-6">
+            <h4><b>Class Groups</b></h4>
+        </div>
+        <div class="col-sm-6">
+            <a href="/class-groups/create" class="btn btn-md btn-success pull-right">Add New Class Group</a>
+        </div>
+    </div>
+    <hr>
 
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <table class="table table-striped table-hover">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Name</th>
-                                <th>Has the following streams</th>
-                                <th>Edit</th>
-                                <th>Delete</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($class_groups as $class_group)
-                            <tr>
-                                <td>{{ $class_group->id }}</td>
-                                <td>{{ $class_group->name }}</td>
-                                <td>
-                                    @foreach ($class_group->streams  as $stream)
-                                        <li>{{ $stream->name }}</li>
-                                    @endforeach
-                                </td>
-                                <td><a href="/class-groups/edit/{{ $class_group->id }}" class="btn btn-primary">Edit</a></td>
-                                <td><a href="/class-groups/delete/{{ $class_group->id }}" class="btn btn-danger">Delete</a></td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Has the following streams</th>
+                        <th>
+                            <p class="pull-right">Action</p>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach ($class_groups as $class_group)
+                    <tr>
+                        <td>{{ $class_group->name }}</td>
+                        <td>
+                            @foreach ($class_group->streams  as $stream)
+                                <span>{{ $stream->name }}</span><br>
+                            @endforeach
+                        </td>
+                        <td>
+                            <div class="btn-group pull-right">
+                                <a href="/class-groups/edit/{{ $class_group->id }}" class="btn btn-default">Edit</a>
+                                <a href="/class-groups/delete/{{ $class_group->id }}" class="btn btn-default">Delete</a>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
