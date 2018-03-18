4c53646161d4af1574f0b0cfdd862666f5e4b03e
mwakaambrose <ambrosemwaka@gmail.com>
2018-03-18 15:34:30 +0300

    prepares print assets and layout

--- resources/views/examinations/results/view.blade.php
+++ resources/views/examinations/results/view.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
+    <div class="row no-print">
         <div class="col-sm-6">
-            <h3 class="text-info">View results for {{ $examination->name }}</h3>
+            <h3 class="text-info">
+                <strong>{{ $student->first_name }}s</strong> 
+                results for 
+                <strong>{{ $examination->name }}</strong>
+            </h3>
         </div>
         <div class="col-sm-6">
         	<a href="/students/{{ $student->id }}" class="btn btn-default pull-right">
@@ -11,10 +15,16 @@
         	</a>
         </div>
     </div>
-    <hr>
+    <hr class="no-print">
     <div class="panel panel-default">
+        <div class="panel-heading">
+            
+        </div>
         <div class="panel-body">
             <code>{{ $student->results }}</code>
+        </div>
+        <div class="panel-footer">
+            
         </div>
     </div>
 @endsection
